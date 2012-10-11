a679870e3019ff002f0359f76a424642a6ad54bb
eudes.andrade <eudes.andrade@gmail.com>
2012-10-11 11:41:28 -0300

    Adicionando configuração de repositório da forgerock.

--- java/autentication-module-senha-rede/lib/mvn-install.sh
+++
@@ -1,2 +0,0 @@
-mvn install:install-file -Dfile=./amserver.jar -DgroupId=org.forgerock.openam -DartifactId=amserver -Dversion=10.0.0 -Dpackaging=jar
-mvn install:install-file -Dfile=./opensso-sharedlib.jar -DgroupId=org.forgerock.openam -DartifactId=opensso-sharedlib -Dversion=10.0.0 -Dpackaging=jar
