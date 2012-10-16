5905398ab01b2153b1196510422d69fe5de07420
eudes.andrade <eudes.andrade@gmail.com>
2012-10-16 11:26:37 -0300

    Implementação do autenticador OpenAM

--- java/single-sign-on-client/src/main/java/br/gov/serpro/openam/singlesingonclient/security/OpenAMAuthenticator.java
+++ java/single-sign-on-client/src/main/java/br/gov/serpro/openam/singlesingonclient/security/OpenAMAuthenticator.java
@@ -2,79 +2,82 @@ package br.gov.serpro.openam.singlesingonclient.security;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 import br.gov.frameworkdemoiselle.security.Authenticator;
 import br.gov.frameworkdemoiselle.security.User;
+import br.gov.frameworkdemoiselle.util.Beans;
 
 /**
- * Autenticador que utiliza o serviço de SSO do OpenAM como mecanismo para verificação
- * de usuários.
+ * Autenticador que utiliza o serviço de SSO do OpenAM como mecanismo para verificação de usuários.
  * 
  * @author Eudes Andrade
- *
  */
 @SessionScoped
 public class OpenAMAuthenticator implements Authenticator {
 
 	private User user;
-	
+
 	@Inject
 	private Credential credential;
-	
+
+	@Inject
+	private OpenAMAuthenticatorClient amAuthenticatorClient;
+
 	@Override
 	public boolean authenticate() {
-		
-		if (credential.getLogin().equalsIgnoreCase("eudes") && credential.getPassword().equalsIgnoreCase("1234")) {
-			
-			this.user = this.createUser();
-			
-			return true;
-			
-		} else {
-			
+
+		try {
+
+			return amAuthenticatorClient.login(credential.getLogin(), credential.getPassword());
+
+		} catch (Exception e) {
 			return false;
-			
 		}
-		
+
 	}
 
 	@Override
 	public void unAuthenticate() {
-		
-		
+
 	}
 
 	@Override
 	public User getUser() {
-		
-		return user;
-		
+
+		if (amAuthenticatorClient.checkLogin(Beans.getReference(HttpServletRequest.class))) {
+			this.user = this.createUser();
+		} else {
+			this.user = null;
+		}
+
+		return this.user;
+
 	}
 
 	private User createUser() {
-		
+
 		return new User() {
-			
+
 			@Override
 			public void setAttribute(Object key, Object value) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public String getId() {
 				// TODO Auto-generated method stub
 				return null;
 			}
-			
+
 			@Override
 			public Object getAttribute(Object key) {
 				// TODO Auto-generated method stub
 				return null;
 			}
 		};
-		
-	}
-	
-}
 
+	}
+
+}
